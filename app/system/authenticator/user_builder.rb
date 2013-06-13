f333b72e14c608b27d7e8633bd6b8de45e2becab
Rahoul Baruah <rahoul@3hv.co.uk>
2013-06-13 15:37:21 +0100

    Bugfix and allow non-admins to create users

--- app/system/authenticator/user_builder.rb
+++ app/system/authenticator/user_builder.rb
@@ -4,12 +4,12 @@ require_relative './security_breach'
 module Authenticator
   class UserBuilder < OpenStruct
     def start
-      raise SecurityBreach.new if account.nil? || (account == :false) || !account.admin?
+      raise SecurityBreach.new if account.nil? || (account == :false) 
       yield user_storage.new
     end
 
     def create_using params
-      raise SecurityBreach.new if account.nil? || (account == :false) || !account.admin?
+      raise SecurityBreach.new if account.nil? || (account == :false)
       yield user_storage.create!(params)
     end
 
